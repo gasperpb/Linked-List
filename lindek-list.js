51d5d17493b8205cb698ab6357219c5b44aceb01
gasperpb <achillespb@hotmail.com>
2020-08-13 23:03:05 -0300

    first comimt

---
+++ lindek-list.js
@@ -0,0 +1,20 @@
+// Lindek-list
+function LindekList() {
+    let head = null
+    let length = 0
+
+    const add = (value) => {
+        if (head) {
+
+        }
+    }
+
+
+    return {
+
+        length: () => length
+    }
+}
+const list = LindekList()
+
+console.log(list.length())
